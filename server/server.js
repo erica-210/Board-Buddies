0f1a70b9456959010754d809070a67b861e951d9
erica-210 <137020409+erica-210@users.noreply.github.com>
2024-02-05 21:51:05 -0600

    added expressmiddleware require

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 //import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require("path");
 
 const { typeDefs, resolvers } = require("./schemas");
