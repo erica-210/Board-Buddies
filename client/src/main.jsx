5bf8c1872e63be66ab5b76f69306dfc3d8e2c6ed
erica-210 <137020409+erica-210@users.noreply.github.com>
2024-02-06 20:48:48 -0600

    building out app and main and adding packages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,27 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import App from './App.jsx';
+import Home from './Home.jsx';
+import SavedGames from './SavedGames.jsx';
+import SearchGames from './SearchGames.jsx';
+import SingleComment from './singleComment.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/saved", element: <SavedGames /> },
+      { path: "/search", element: <SearchGames /> },
+      { path: "/comments", element: <SingleComment /> },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router} />
 )
