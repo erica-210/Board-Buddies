fa8a282d0da5d5fa7a593029acbc0c3085c99e63
erica-210 <137020409+erica-210@users.noreply.github.com>
2024-02-12 17:30:04 -0600

    fixing issue

--- client/src/components/AnimeToWatch/index.jsx
+++ client/src/components/AnimeToWatch/index.jsx
@@ -1,59 +1,39 @@
 import React, { useState, useEffect } from "react";
-import { getSavedAnimeIds, saveAnimeIds } from "../../utils/localStorage";
+import { Link } from "react-router-dom";
+import { getSavedAnimeData } from "../../utils/localStorage";
+import { Select } from 'antd';
 
-const AnimeToWatch= () => {
+const { Option } = Select;
+
+const AnimeToWatch = () => {
   // State to store the list of anime watched
-  const [animeToWatch, setAnimeToWatch] = useState([]);
+  const [planToWatchAnime, setPlanToWatchAnime] = useState([]);
 
   // State to store the selected anime in the dropdown
   const [selectedAnimeId, setSelectedAnimeId] = useState("");
 
   // Load anime watched from local storage on component mount
   useEffect(() => {
-    const storedAnimeToWatch = getSavedAnimeIds();
-    console.log("Stored anime IDs:", storedAnimeToWatch);
-    setAnimeToWatch(storedAnimeToWatch);
+    const savedAnimeData = getSavedAnimeData();
+    console.log("Saved anime data:", savedAnimeData);
+    setPlanToWatchAnime(savedAnimeData.planToWatch);
   }, []);
 
-  // Function to handle removing an anime from the watched list
-  const handleRemoveAnime = (animeId) => {
-    const updatedAnimeToWatch = animeToWatch.filter(
-      (savedAnimeId) => savedAnimeId !== animeId
-    );
-    console.log("Updated anime to watch after removal:", updatedAnimeToWatch);
-    setAnimeToWatch(updatedAnimeToWatch);
-    saveAnimeIds(updatedAnimeToWatch);
+  const handleSelectChange = (value) => {
+    setSelectedAnimeId(value);
   };
-
-  // Log selected anime ID when it changes
-  useEffect(() => {
-    console.log("Selected anime ID:", selectedAnimeId);
-  }, [selectedAnimeId]);
 
   return (
     <div>
       <h2>Watch Next</h2>
-      <select
-        value={selectedAnimeId}
-        onChange={(e) => setSelectedAnimeId(e.target.value)}
-      >
-        <option value="">Select an anime</option>
-        {/* Map over anime watched to generate options */}
-        {animeToWatch.map((animeId, index) => (
-          <option key={index} value={animeId}>
-            {animeId}
-          </option>
+      <Select value={selectedAnimeId} onChange={handleSelectChange} style={{ width: 200 }}>
+        <Option value="">Select an anime</Option>
+        {planToWatchAnime.map((animeId, index) => (
+          <Option key={index} value={animeId}>
+            <Link to={`/anime/${animeId}`}>{animeId}</Link>
+          </Option>
         ))}
-      </select>
-
-      <ul>
-        {animeToWatch.map((animeId, index) => (
-          <li key={index}>
-            {animeId} {/* Display anime information here */}
-            <button onClick={() => handleRemoveAnime(animeId)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+      </Select>
     </div>
   );
 };
