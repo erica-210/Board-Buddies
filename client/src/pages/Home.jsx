fa5982e5feb62ea86dbf734ebcc08f56f49aa8fd
erica-210 <137020409+erica-210@users.noreply.github.com>
2024-02-12 21:14:26 -0600

    finishing up the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import Login from '../components/LoginForm/Login';
-import Signup from '../components/SignupForm/index';
-import { Typography } from "antd";
+import Login from "../components/LoginForm/Login";
+import Signup from "../components/SignupForm/index";
+import { Row, Typography, Col } from "antd";
+import meme from "../assets/code-meme.jpg";
 
 const { Title } = Typography;
 
-
 const Home = () => {
   return (
-    <div style={{ textAlign: "center" }}>
-    <div>
-      <div className="welcome-container" >
-      <h1 className="welcome" >Welcome to the anime discovery hub!</h1>
+    <div className="homepage">
+      <div>
+        <div className="welcome-container">
+          <h1 className="welcome">Welcome to the anime discovery hub!</h1>
+        </div>
+        <div className="login-signup">
+          <Row>
+            <div>
+              <Col span={18} className="meme">
+                <img
+                  id="meme"
+                  src={meme}
+                  alt="meme"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </Col>
+            </div>
+
+            <Col span={6} className="forms">
+              <Login />
+              <Signup />
+            </Col>
+          </Row>
+        </div>
       </div>
-      <Login />
-      <Signup />
-    </div>
     </div>
   );
 };
--- client/src/pages/SearchAnime.jsx
+++ client/src/pages/SearchAnime.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 
 import SearchAnimeForm from "../components/SearchAnimeForm";
-import AnimeDetails from "../components/AnimeDetail";
 
 const SearchAnimes = () => {
   return (
     <div>
-      <div className="welcome-container" >
-      <h1 className="welcome" >Welcome to the Search Anime Page</h1>
+      <div className="welcome-container">
+        <h1 className="welcome">Welcome to the Search Anime Page</h1>
       </div>
       <SearchAnimeForm />
-      <AnimeDetails />
     </div>
   );
 };
